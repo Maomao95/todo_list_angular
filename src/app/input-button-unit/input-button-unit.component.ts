341cebede5403ef541bf11f5ad9daecbf83e9bb2
Maomao95 <my1876@nyu.edu>
2020-08-19 13:25:52 -0400

    first time

---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  templateUrl: './input-button-unit.component.html',
+  styleUrls: ['./input-button-unit.component.css']
+})
+export class InputButtonUnitComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  title = "Hello World"
+}
